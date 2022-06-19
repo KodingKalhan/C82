543946043dae79cef1b07a302e96797744e3bd07
KodingKalhan <94335663+KodingKalhan@users.noreply.github.com>
2022-06-19 19:06:22 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var mouseEvent = "empty";
+var last_position_of_x, last_position_of_y;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "black";
+    width_of_line = 1;
