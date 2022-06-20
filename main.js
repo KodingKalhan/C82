a71009ea5ee2a204f821279fd86467fb65362ba9
KodingKalhan <94335663+KodingKalhan@users.noreply.github.com>
2022-06-19 22:52:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 var mouseEvent = "empty";
-var last_position_of_x, last_position_of_y;
+var last_position_of_x, last_position_of_y; 
+canvas = document.getElementById('myCanvas'); 
+ctx = canvas.getContext("2d"); color = "black"; 
+width_of_line = 1;
 
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    width_of_line = 1;
+canvas.addEventListener("mouseup", my_mouseup);
+function my_mouseup(e)
+{
+    mouseEvent = "mouseUP";
+}
+
+canvas.addEventListener("mouseleave", my_mouseleave);
+function my_mouseup(e)
+{
+    mouseEvent = "mouseUP";
+}
