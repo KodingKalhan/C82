4227e8eb1c0c214bbabe55fd8e61bf38c557e197
KodingKalhan <94335663+KodingKalhan@users.noreply.github.com>
2022-06-21 13:25:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,5 +13,18 @@ function my_mouseup(e)
 canvas.addEventListener("mouseleave", my_mouseleave);
 function my_mouseup(e)
 {
-    mouseEvent = "mouseUP";
+    mouseEvent = "mouseleave";
 }
+
+canvas.addEventListener("mousemove", my_mousemove); function my_mousemove(e)
+current_position_of_mouse_x = e.clientX - canvas.offsetLeft; current_position_of_mouse_y = e.clienty - canvas.offsetTop;
+if (mouseEvent == "mouseDown") { ctx.beginPath(); ctx.strokeStyle = color; ctx. lineWidth = width_of_line;
+console.log("Last position of x and y coordinates = "); console.log("x = " + last_position_of_X + "y = " + last_position_of_y); ctx.moveTo(last_position_of_x, last_position_of_y);
+console.log("Current position of x and y coordinates = "); console.log("x = " + current_position_of_mouse_X + "y = " + current_position_of_mouse_y); ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y); ctx.stroke();
+last_position_of_x = current_position_of_mouse_x; last_position_of_y = current_position_of_mouse_y;
+                                
+console.log("Last position of x and y coordinates = "); 
+console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+                                
+console.log("Current position of x and y coordinates = "); console.log("x = " + current position of mouse x + "y = " + current position of mouse_y):
+
